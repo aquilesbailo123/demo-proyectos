67fe71709a579a8966b5102e0ef05eb92ec37e65
Aquiles Bailo <aquiles.4.bailo@gmail.com>
2025-04-13 02:06:53 -0500

    Fixing import

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,7 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-import common from '@/locales/common'
+import common from '../src/locales/common'
 
 i18n
     .use(initReactI18next)
